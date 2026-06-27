8a6c9cb595a572f21349160192e1f684baec4a41
ARafay007 <rafay_007@outlook.com>
2026-06-27 09:34:38 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+import express from 'express';
+import cors from 'cors';
+import { rateLimit } from 'express-rate-limit'
+import { connectDB } from './connectDB.js';
+import { studentRoutes } from './routes/student.js';
+import { teacherRoutes } from './routes/teacher.js';
+import 'dotenv/config'
+
+await connectDB();
+
+const app = express();
+
+app.use(cors());
+
+const limiter = rateLimit({
+  windowMs: 0.1 * 60 * 1000, // 10 milliseconds time window
+  limit: 2, // Max 100 requests per IP per window
+  message: 'Too many requests from this IP, please try again later.',
+  statusCode: 429, // Explicitly return 429 Too Many Requests
+  standardHeaders: 'draft-8', // Return standard IETF rate-limit headers
+  legacyHeaders: false, // Turn off X-RateLimit-* headers
+});
+
+app.use(limiter);
+
+app.use(express.json());
+
+app.use('/student', studentRoutes);
+
+app.use('/teacher', teacherRoutes);
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`server is listening at http://localhost:${PORT}`);
+});
